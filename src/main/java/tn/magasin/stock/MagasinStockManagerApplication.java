6dde353fb2a4df6c6ab4d449cd26aeff6b32f8c0
monambengouta <monam.bengouta@esprit.tn>
2021-10-09 18:27:25 +0100

    add project to github

---
+++ src/main/java/tn/magasin/stock/MagasinStockManagerApplication.java
@@ -0,0 +1,13 @@
+package tn.magasin.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagasinStockManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagasinStockManagerApplication.class, args);
+	}
+
+}

574248ca957f307bf64a15abcb41176f2f3651df
wajdi0307 <78967021+wajdi0307@users.noreply.github.com>
2021-11-28 20:52:36 +0100

    push-after-conflicts

--- src/main/java/tn/magasin/stock/configuration/SwaggerConfig.java
+++ src/main/java/tn/magasin/stock/configuration/SwaggerConfig.java
@@ -3,8 +3,10 @@ package tn.magasin.stock.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -18,7 +20,13 @@ public class SwaggerConfig {
 				.select()
 				.apis(RequestHandlerSelectors.any())
 				.paths(PathSelectors.any())
-				.build();
+				.build().apiInfo(apiInfo());
+	}
+	private ApiInfo apiInfo () {
+		return new ApiInfoBuilder()
+				.title("Configuration swagger ppour le proje tSpring")
+				.description("\"Configuration de Swagger\"")
+				.version("1.1.0").build();
 	}
 
 }

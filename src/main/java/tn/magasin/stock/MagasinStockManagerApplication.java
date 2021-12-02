bd3651d5e35a4963699ddad19306bc6ba594da58
AchrefBarketi <achref.barketi@esprit.tn>
2021-12-02 15:47:32 +0100

    changes in user test

--- src/main/java/tn/magasin/stock/MagasinStockManagerApplication.java
+++ src/main/java/tn/magasin/stock/MagasinStockManagerApplication.java
@@ -31,7 +31,7 @@ public class MagasinStockManagerApplication {
         return new BCryptPasswordEncoder();
     }
 
-    /*@Bean
+    @Bean
     CommandLineRunner run(UserService us) {
         return args -> {
             us.saveRole(new Role(null, "ROLE_USER"));
@@ -55,7 +55,6 @@ public class MagasinStockManagerApplication {
 
         };
 
-
-    }*/
+    }
 
 }
